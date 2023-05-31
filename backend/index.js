fe0294b53a58968c221fcaa568df02300111151a
Awwal <jubral14@gmail.com>
2023-06-01 00:26:58 +0100

    backend pushing
    working on openai requests, hope not be long

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+require("dotenv").config();
+const { Configuration, OpenAIApi } = require("openai");
+
+const app = express();
+
+app.use(express.json());
+
+const configuration = new Configuration({
+    apiKey: process.env.OPEN_AI_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+app.post("/test", async (req, res) => {
+    try {
+        return res.status(200).json({
+            message: "Working, Server is up",
+        });
+    } catch (error) {}
+})
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Server is listening on Port ${port}`));
