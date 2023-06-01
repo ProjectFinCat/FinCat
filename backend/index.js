370f2936365c047e816db49184da287440e8393e
Awwal <jubral14@gmail.com>
2023-06-01 01:12:16 +0100

    final structure

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,11 @@ const openai = new OpenAIApi(configuration);
 
 app.post("/test", async (req, res) => {
     try {
+        // const response = await openai.createCompletion({
+            // model: "gpt-3.5",
+            // prompt: ``,
+        // });
+
         return res.status(200).json({
             message: "Working, Server is up",
         });
