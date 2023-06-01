f8bca134c513b2b59e15c7bc3597fb19e51c6695
Awwal <jubral14@gmail.com>
2023-06-01 01:32:28 +0100

    another restructure

--- backend/index.js
+++ backend/index.js
@@ -24,6 +24,28 @@ app.post("/test", async (req, res) => {
     } catch (error) {}
 })
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => console.log(`Server is listening on Port ${port}`));
+
+// import { usePlaidLink } from 'react-plaid-link';
+// import { Configuration, PlaidApi, PlaidEnvironments } from 'plaid';
+
+// const configuration = new Configuration({
+//   basePath: PlaidEnvironments.sandbox,
+//   baseOptions: {
+//     headers: {
+//       'PLAID-CLIENT-ID': '64768fb8d454430013b3997d',
+//       'PLAID-SECRET': 'eaac864fd7c6fac49e577ea098cc21',
+//     },
+//   },
+// });
+
+// const client = new PlaidApi(configuration);
+// function App() {
+//   const { open, ready } = usePlaidLink({
+//   token: '<GENERATED_LINK_TOKEN>',
+//   onSuccess: (public_token, metadata) => {
+//     // send public_token to server
+//   },
+// });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
-import { usePlaidLink } from 'react-plaid-link';
-import { Configuration, PlaidApi, PlaidEnvironments } from 'plaid';
 
-const configuration = new Configuration({
-  basePath: PlaidEnvironments.sandbox,
-  baseOptions: {
-    headers: {
-      'PLAID-CLIENT-ID': '64768fb8d454430013b3997d',
-      'PLAID-SECRET': 'eaac864fd7c6fac49e577ea098cc21',
-    },
-  },
-});
-
-const client = new PlaidApi(configuration);
 function App() {
-  const { open, ready } = usePlaidLink({
-  token: '<GENERATED_LINK_TOKEN>',
-  onSuccess: (public_token, metadata) => {
-    // send public_token to server
-  },
-});
+
   return (
     <div className="App">
       
@@ -37,9 +19,7 @@ function App() {
         >
           Learn React
         </a>
-         <button onClick={() => open()} disabled={!ready}>
-      Connect a bank account
-    </button>
+
       </header>
      
     </div>
